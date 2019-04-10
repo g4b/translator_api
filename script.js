45a971dc9deb6518896573b818f67c7a42c66162
gabe fantacone <gabrielfantacone@students.berkeley.net>
2019-04-10 14:12:07 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $.ajax({
+        url: "https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20190410T205210Z.9361e13ac5785266.c7" +
+        "61d9002a2bc9c5fbc3d5849d8b0eda0f5e305e",
+        type: 'POST',
+        success: createSelects
+    })
+});
+
+function translate(){
+    var text = $("#textInput").val();
+    var lang =
+    $.ajax({
+        url: 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190410T205210Z.9361e13ac5785266.c7' +
+        '61d9002a2bc9c5fbc3d5849d8b0eda0f5e305e&text=' + text + '&lang=' + lang,
+        type: 'POST',
+        success: printResult
+    })
+}
+
+function createSelects(result){
+    for (var i = 0; i < result.length; i++){
+        var option = $("option");
+        option.val(i);
+        option.html(result.langs[i]);
+        $("#select").append(option);
+    }
+}
+
+function printResult(result){
+    
+}
